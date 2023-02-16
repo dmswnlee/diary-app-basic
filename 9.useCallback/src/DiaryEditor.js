ad9cfa5ab7d90a7d9ad8421998a917209f04b3c1
dmswnlee <dlrmawnl03@gmail.com>
2023-02-16 20:18:54 +0900

    useCallback, 최적화

---
+++ 9.useCallback/src/DiaryEditor.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+const DiaryEditor = ({onCreate}) => {
+
+   useEffect(() => {
+      console.log('DiaryEditor 렌더');
+   });
+
+    const authorInput = useRef();
+    const contentInput = useRef();
+
+   const [state, setState] = useState({
+      author: '',
+      content: '',
+      emotion: 1, 
+   });
+
+   
+
+   const handleChangeState = e => {
+      setState({
+         ...state,
+         [e.target.name]: e.target.value,
+      });
+   };
+
+   const handleSubmit = () => {
+    if(state.author.length < 1){
+        authorInput.current.focus();
+        return;
+    }
+
+    if(state.content.length < 5){
+        contentInput.current.focus();
+        return;
+    }
+    onCreate(state.author, state.content, state.emotion);
+    alert('저장 성공');
+    setState({
+        author: '',
+        content: '',
+        emotion: 1, 
+    });
+   };
+
+   return (
+      <div className="diaryEditor">
+         <div>
+            <h2>오늘의 일기</h2>
+            <input 
+                ref={authorInput}
+                type="text" 
+                name='author'
+                value={state.author} 
+                onChange={handleChangeState} 
+            />
+         </div>
+         <div>
+            <textarea 
+                ref={contentInput}
+                name='content'
+                value={state.content} 
+                onChange={handleChangeState} 
+            />
+         </div>
+         <div>
+            <select 
+                name='emotion' 
+                value={state.emotion} 
+                onChange={handleChangeState}
+            >
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+            </select>
+         </div>
+         <div>
+            <button onClick={handleSubmit}>일기 저장하기</button>
+         </div>
+      </div>
+   );
+};
+
+export default React.memo(DiaryEditor); // 이렇게 최적화 시켜줘도 됨 
